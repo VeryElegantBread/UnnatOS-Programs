a1f5569cf4e815d9185f93685c4b4dd62c430805
VeryElegantBread <simon.saucerzeoli@gmail.com>
2025-03-04 16:12:40 -0500

    added fortune

---
+++ fortune.lua
@@ -0,0 +1,18 @@
+local fortune_name = SplitString(Input, " ")[1]
+local fortune_path = {"System", "Programs", fortune_name}
+if not item_exists(fortune_path) then
+	fortune_path = StringToPath(fortune_name)
+end
+
+local fortunes_list_path = fortune_path
+table.insert(fortunes_list_path, "fortunes")
+if not item_exists(fortunes_list_path) then
+	new_item(fortunes_list_path)
+	set_text(fortunes_list_path, get_data("https://raw.githubusercontent.com/bmc/fortunes/master/fortunes"))
+end
+
+local fortunes_list_text = get_text(fortunes_list_path)
+
+local fortunes_list = SplitString(fortunes_list_text, "\n%%\n")
+
+return { fortunes_list[math.random(1, #fortunes_list)] }
